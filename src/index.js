74559732222460c8dffcd58c116ceeb64514ce95
Ahlamonepiece1989 <Ahlam.Daham.rae.js@tuwaiq.edu.sa>
2021-12-18 17:58:22 +0300

    frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 ReactDOM.render(
-    <BrowserRouter>
+  <BrowserRouter>
     <App />
-    </BrowserRouter>,
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
