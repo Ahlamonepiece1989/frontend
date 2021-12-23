a7cb5d6663211472b8df21af3b7d8f55ca192e74
Ahlamonepiece1989 <Ahlam.Daham.rae.js@tuwaiq.edu.sa>
2021-12-23 10:50:00 +0300

    frontend

---
+++ src/components/Dropdown/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems =[
+
+    {
+        title: 'PDF to Word',
+        path: '/pdftoword',
+        cName: 'dropdown-link'
+      },
+      {
+        title: 'Word to PDF',
+        path: '/wordtopdf',
+        cName: 'dropdown-link'
+      }
+
+
+
+
+
+
+
+
+
+];
